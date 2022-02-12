e54f2d6404f9f12ba94801738b41e1f52711e968
yasumasa ono <yasumasa.ono0725@gmail.com>
2022-02-12 16:09:07 +0900

    jsファイル関数の呼び出し順を修正

--- js/common.js
+++ js/common.js
@@ -1,21 +1,23 @@
 $(function () {
-    $(document).ready(function () {
-        $('.drawer').drawer();
-    });
+  
+  AOS.init();
+  
+  $('.main-visual').vegas({
+    overlay: false,
+    transition: 'blur', 
+    transitionDuration: 2000,
+    delay: 10000,
+    animation: 'kenburns',
+    animationDuration: 10000,
+    slides: [
+      { src: './img/photo01.jpg' },
+      { src: './img/photo011.jpg' },
+      { src: './img/photo012.jpg' },
+    ]
+  });
 
-    AOS.init();
+  $(document).ready(function () {
+      $('.drawer').drawer();
+  });
 
-    $('.main-visual').vegas({
-      overlay: false,
-      transition: 'blur', 
-      transitionDuration: 2000,
-      delay: 10000,
-      animation: 'kenburns',
-      animationDuration: 10000,
-      slides: [
-        { src: './img/photo01.jpg' },
-        { src: './img/photo011.jpg' },
-        { src: './img/photo012.jpg' },
-      ]
-    });
 })
